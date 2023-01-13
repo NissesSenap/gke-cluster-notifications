226077e11619f703063fbf63f10dec64f0bd2dd7
David Alger <davidmalger@gmail.com>
2023-01-13 13:06:12 -0600

    Support JSON formatted Stackdriver compatible logging

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@ use axum::routing::{get, post};
 use axum::{Json, Router};
 use serde_json::Value;
 use std::{env, net::SocketAddr, str::FromStr};
-use tracing::{info, Level};
+use tracing::{debug, info, Level};
 use tracing_subscriber::{prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(EnvFilter::builder().with_default_directive(Level::INFO.into()).from_env_lossy())
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    let env_filter =
+        EnvFilter::builder().with_default_directive(Level::INFO.into()).from_env_lossy();
+
+    if env_or_default("JSON_LOG", "false").expect("JSON_LOG should be true or false") {
+        tracing::subscriber::set_global_default(
+            tracing_subscriber::registry().with(env_filter).with(tracing_stackdriver::layer()),
+        )
+        .expect("failed to set global default subscriber");
+    } else {
+        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+    }
 
     let listen_addr = SocketAddr::new(
         env_or_default("HOST", "0.0.0.0").expect("LISTEN_HOST should be an IP address"),
@@ -33,6 +40,6 @@ fn env_or_default<F: FromStr>(key: &str, default: &str) -> Result<F, F::Err> {
     env::var(key).unwrap_or_else(|_| default.to_string()).parse()
 }
 
-async fn handler(payload: Json<Value>) -> Json<Value> {
-    payload
+async fn handler(Json(payload): Json<Value>) {
+    debug!(payload = serde_json::to_string(&payload).unwrap(), "message received");
 }
